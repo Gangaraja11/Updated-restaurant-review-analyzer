a73908d494d7ccbbf803e7b5d826cd0680e56cdd
Gangaraja B <156883042+Gangaraja11@users.noreply.github.com>
2025-11-15 11:01:33 +0530

    Jstry

--- script.js
+++ script.js
@@ -683,15 +683,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Mobile Menu Toggle
 const hamburger = document.getElementById("hamburger");
-
 const navLinks = document.getElementById("navLinks");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navLinks.classList.toggle("active");
 });
-
-
 
 
 
